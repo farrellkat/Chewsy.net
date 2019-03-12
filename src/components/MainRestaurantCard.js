c5d8f4cff84a133585783c0c2e10646d51023b20
James Farrell <jamesfarrellmusic@gmail.com>
2019-03-12 15:05:26 -0500

    some styling for the main card

--- src/components/MainRestaurantCard.js
+++ src/components/MainRestaurantCard.js
@@ -6,22 +6,37 @@ export default class MainRestaurantCard extends Component {
         return (
             <React.Fragment>
                 <section className="restaurantInfoCardContainer">
-                <div className="restaurantInfoImageContainer">
-                    <img
-                    src={this.props.businessInfo[0].image_url}
-                    alt="restaurant"
-                    rel="noopener noreferrer"
-                    className="restaurantInfoImage"></img>
+                    <div className="restaurantInfoImageContainer">
+                        <img
+                            src={this.props.businessInfo[0].image_url}
+                            alt="restaurant"
+                            rel="noopener noreferrer"
+                            className="restaurantInfoImage"></img>
                     </div>
                     <div className="restaurantInfoContainer">
-                <h1>{this.props.businessInfo[0].name}</h1>
-                <div>Rating: {this.props.businessInfo[0].rating}/5</div>
-                <div>Price: {this.props.businessInfo[0].price}</div>
-                {/* <div>location: {this.props.businessInfo[0].location}</div> */}
-                <div><a href={this.props.businessInfo[0].url} target="_blank">Visit {this.props.businessInfo[0].name} on Yelp.</a></div>
-                </div>
+                        <h1>{this.props.businessInfo[0].name}</h1>
+                        <div className="MainCardAddressContainer">
+                            <div>Address:</div>
+                            <div>
+                                {this.props.businessInfo[0].location.address1}
+                            </div>
+                            <div>
+                                {this.props.businessInfo[0].location.city},&nbsp;
+                                {this.props.businessInfo[0].location.state}&nbsp;
+                                {this.props.businessInfo[0].location.zip_code}
+                            </div>
+                        </div>
+                        <div className="additionalInfo">
+                            <div>Phone Number: {this.props.businessInfo[0].display_phone}</div>
+                            <div>Business Website: <a href={this.props.businessInfo[0].url} target="_blank">{this.props.businessInfo[0].name}</a></div>
+                        </div>
+                        <div className="ratingAndPrice">
+                            <div>Rating: {this.props.businessInfo[0].rating}/5</div>
+                            <div>Price: {this.props.businessInfo[0].price}</div>
+                        </div>
+                    </div>
                 </section>
-            </React.Fragment>
+            </React.Fragment >
         )
     }
 }
