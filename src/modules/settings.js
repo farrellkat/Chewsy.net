de40b6bc2bfad04a420014a48922fcdb955d714e
James Farrell <jamesfarrellmusic@gmail.com>
2019-04-02 20:21:04 -0500

    changed settings to github

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -1,5 +1,5 @@
 export default {
     yelpAPI: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
     localURL: "http://localhost:5002",
-    appDataURL: "https://choose-chewsy.herokuapp.com"
+    appDataURL: "https://github.com/jesperorb/json-server-heroku.git"
   }
