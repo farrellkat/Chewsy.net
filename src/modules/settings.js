7a3d2e7c8c163d4fcac3702c8be8dda8996ff7a7
James Farrell <jamesfarrellmusic@gmail.com>
2019-05-01 16:39:54 -0500

    made settings https

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -2,6 +2,6 @@ const port = process.env.PORT || 4000;
 
 export default {
     yelpAPI: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
-    appDataURL: "http://api.chewsy.net",
+    appDataURL: "https://api.chewsy.net",
     // appDataURL: "https://heroku-chewsy-api.herokuapp.com"
   }
