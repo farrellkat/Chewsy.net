0929956f74e0a9b941d7cd7b6016f5c0b7bf8101
James Farrell <jamesfarrellmusic@gmail.com>
2019-05-01 16:15:34 -0500

    changed callback to www

--- src/components/authentication/auth0-variables.js
+++ src/components/authentication/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
     domain: 'dev-cerpmdij.auth0.com',
     clientId: 'SjziF5SksNOB91J0qxRft6sARYUcwIgn',
-    callbackUrl: 'https://chewsy.net/callback'
+    callbackUrl: 'https://www.chewsy.net/callback'
   }
