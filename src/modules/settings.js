dada55fffa38e9bdb985ffb607e3afdee303a810
James Farrell <jamesfarrellmusic@gmail.com>
2019-05-02 08:41:38 -0500

    fixed api to cors anywhere

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -2,6 +2,6 @@ const port = process.env.PORT || 4000;
 
 export default {
     yelpAPI: "https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3",
-    appDataURL: "https://cors-escape.herokuapp.com/https://api.chewsy.net",
+    appDataURL: "https://cors-anywhere.herokuapp.com/https://api.chewsy.net",
     // appDataURL: "https://heroku-chewsy-api.herokuapp.com"
   }
